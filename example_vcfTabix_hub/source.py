79617632c56e3327c905d8e410c580ece72a0347
GitHub Action <action@github.com>
2021-06-01 17:47:48 +0000

    update hub

--- example_vcfTabix_hub/source.py
+++ example_vcfTabix_hub/source.py
@@ -1,19 +1,23 @@
 import trackhub
 
-hub, genome, genomes_file,trackdb = trackhub.default_hub(
-    hub_name = 'vcfTabix',
-    short_label = 'vcfTabix_example',
-    long_label = 'vcfTabix_example',
-    genome = 'hg19',
-    defaultPos = 'chr21:33034804-33037719',
-    email = 'eva.jason@nih.gov')
+hub, genome, genomes_file, trackdb = trackhub.default_hub(
+    hub_name="vcfTabix",
+    short_label="vcfTabix",
+    long_label="vcfTabix",
+    genome="hg19",
+    defaultPos="chr21:33034804-33037719",
+    email="eva.jason@nih.gov",
+)
 
 track = trackhub.Track(
-    tracktype = 'vcfTabix',
-    name = 'vcfTabix',
-    chromosomes = 'chr21',
-    visibility = 'pack',
-    bigDataUrl = 'http://genome.ucsc.edu/goldenPath/help/examples/vcfExample.vcf.gz')
+    tracktype="vcfTabix",
+    name="VCF_Example_One",
+    chromosomes="chr21",
+    visibility="pack",
+    url="http://genome.ucsc.edu/goldenPath/help/examples/vcfExample.vcf.gz",
+)
 trackdb.add_tracks(track)
 
-trackhub.upload.upload_hub(hub = hub, host = 'localhost',remote_dir = 'example_hubs/example_vcfTabix_hub')
+trackhub.upload.upload_hub(
+    hub=hub, host="localhost", remote_dir="example_hubs/example_vcfTabix_hub"
+)
